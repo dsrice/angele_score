9afc038fb7658f9cffbb59c525b7de985d2a3656
dsrice <v.fuji.0223@gmail.com>
2020-09-17 00:35:06 +0900

    設定が書き換わっていたので修正

--- bowring/models.py
+++ bowring/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
-from django.utils import timezone
 from core.models import TimeStampedModel
 
 # ユーザーマネージャー
--- core/models.py
+++ core/models.py
@@ -8,12 +8,12 @@ class TimeStampedModel(models.Model):
     """
 
     created_at = models.DateTimeField(auto_now_add=True)
-    created_user_id = models.IntegerField()
+    created_user_id = models.IntegerField(blank=True, null=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    updated_user_id = models.IntegerField()
+    updated_user_id = models.IntegerField(blank=True, null=True)
     is_deleted = models.IntegerField(default=0)
-    deleted_at = models.DateTimeField()
-    deleted_user_id = models.IntegerField()
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted_user_id = models.IntegerField(blank=True, null=True)
 
     class Meta:
         abstract = True
