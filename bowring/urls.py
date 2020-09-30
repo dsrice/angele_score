e0be3973d6c762b954208e730f0c1d677cd861af
dsrice <v.fuji.0223@gmail.com>
2020-09-30 23:36:20 +0900

    ログアウト機能を用意

--- bowring/urls.py
+++ bowring/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import homeview, loginview
+from .views import homeview, loginview, logoutview
 
 app_name = "bowring"
 urlpatterns = [
     path('login', loginview.index, name='login'),
     path('home', homeview.get, name='home'),
+    path('logout', logoutview.index, name='logout'),
 ]
