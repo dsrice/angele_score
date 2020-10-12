7aa398a546dafcf39d51dc22768b6f923184ceaa
dsrice <v.fuji.0223@gmail.com>
2020-10-12 23:47:14 +0900

    認証後にJWTを取得してセッションにつめる

--- bowring/views/loginview.py
+++ bowring/views/loginview.py
@@ -3,6 +3,8 @@ from django.views import View
 
 from bowring.forms.loginform import LoginForm
 from django.contrib.auth import login, authenticate
+from core.api import Api_method
+from core import error
 
 
 def index(request):
@@ -11,6 +13,13 @@ def index(request):
         user = authenticate(email=form.data["email"], password=form.data["password"])
         if user:
             login(request, user)
+            api = Api_method()
+            token = api.auth(email=form.data["email"], password=form.data["password"])
+            if not token:
+                return error.handler404(request)
+
+            request.session["token"] = token
+
             return redirect("bowring:home")
 
     return render(request, 'login/index.html', {'form': form})
--- core/api.py
+++ core/api.py
@@ -5,29 +5,28 @@ import environ
 env = environ.Env()
 env.read_env('.env')
 
+
 class Api_method:
     traffic = env('API_TRAFFIC')
     host = env('API_HOST')
     base_url = traffic + "://" + host + "/"
 
-    def auth(self, user):
+    def auth(self, email, password):
         """
         認証処理をおこない、JWTを取得する
         """
         client = APIClient()
         url = self.base_url + "v1/token/"
+        print(url)
         data = {
-            "email": user.email,
-            "password": "test"
+            "email": email,
+            "password": password
         }
-        print(data)
         response = client.post(url, data)
-
         if response.status_code == status.HTTP_200_OK:
             return "JWT " + response.data["token"]
         else:
             return None
-
 
     def get(self, url, token, params):
         client = APIClient()
