7f957539a6b0cfdac6ff0d3855cd20636c849d8b
dsrice <v.fuji.0223@gmail.com>
2020-10-13 00:57:02 +0900

    初期表示時の処理を追加 編集箇所の表示も追加

--- bowring/static/javascript/gamescore.js
+++ bowring/static/javascript/gamescore.js
@@ -18,15 +18,35 @@ function get_score(){
             "event_id": event_id,
             "game_count": gamecount
         }
-    })
-    .done((data) => {
-      //成功した場合の処理
-      console.log(data);
+    }).done((data) => {
+        if(data["score"] == null){
+            // 未入力状態
+            $("#id_framecount").val(1)
+            $("#id_throwcount").val(1)
+        }
+
+        active_frame();
+
     })
     .fail((data) => {
       //失敗した場合の処理
       console.log(data.responseText);  //レスポンス文字列を表示
     })
+}
+
+before_framecount = 1
+before_throwcount = 1
+/*
+編集している投球箇所の表示処理
+ */
+function active_frame(){
+    framecount = $("#id_framecount").val()
+    throwcount = $("#id_throwcount").val()
+
+    target_id = "score_" + framecount + "_" + throwcount
+    before_id = "score_" + before_framecount + "_" + before_throwcount
+    $("[id=" + before_id + "]").removeClass("active_frame")
+    $("[id=" + target_id + "]").addClass("active_frame")
 }
 
 $(window).on("load", function (){
